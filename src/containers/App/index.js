7431989a939e4602f6adf894674d0c288e9062c9
Jace Mitsuka <PJace@BigMac.local>
2018-05-22 14:12:06 -1000

    Use new components

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import logo from '../../logo.svg';
 import './App.css';
 
+import {
+  getBooksFromFakeXHR as getBooks
+} from '../../lib/books.db';
+import BookList from '../../components/BookList';
+
 import AppTitle from '../../components/AppTitle';
+import FeaturedBook from '../../components/FeaturedBook';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      books: []
+    }
+  }
+  componentDidMount() {
+    this.setState({ title: 'My Books List'});
+    getBooks()
+    .then( books => {
+      // this.setState({ books:books})
+      this.setState({ books });
+    });
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-      
+
         </header>
-        <AppTitle title="My Book List"/>
+        <AppTitle title={this.state.title}/>
+        {
+          this.state.books.length > 0 
+          ?  <FeaturedBook book={this.state.books[2]}/>
+          : null
+        }
+        <BookList books={this.state.books}/>
       </div>
     );
   }
